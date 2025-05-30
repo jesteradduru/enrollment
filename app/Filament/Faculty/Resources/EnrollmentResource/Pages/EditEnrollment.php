ad5b32f2dc2ee2f59f6f42dfde5a8ab3ee933038
Jester Adduru <jesteradduru@gmail.com>
2025-05-30 17:46:00 +0800

    Add trapping for enrollment

--- app/Filament/Faculty/Resources/EnrollmentResource/Pages/EditEnrollment.php
+++ app/Filament/Faculty/Resources/EnrollmentResource/Pages/EditEnrollment.php
@@ -3,8 +3,13 @@
 namespace App\Filament\Faculty\Resources\EnrollmentResource\Pages;
 
 use App\Filament\Faculty\Resources\EnrollmentResource;
+use App\Models\Enrollment;
+use App\Models\SchoolYear;
+use App\Models\Student;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class EditEnrollment extends EditRecord
 {
@@ -15,5 +20,49 @@ class EditEnrollment extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        // dd($data);
+        $studentId = $data['student_id'];
+    $schoolYearId = $data['school_year_id'];
+
+    // Check for duplicate enrollment
+    $alreadyEnrolled = Enrollment::where('student_id', $studentId)
+        ->where('school_year_id', $schoolYearId)
+        ->exists();
+
+    if ($alreadyEnrolled) {
+        Notification::make()
+            ->title('Student already enrolled in this school year.')
+            ->danger()
+            ->send();
+
+        $this->halt(); // Stops creation
+        return static::getModel()::create($data);
+    }
+
+    $currentSY = SchoolYear::find($schoolYearId);
+    if (!$currentSY) return static::getModel()::create($data);
+
+    $lastSY = SchoolYear::where('start_year', $currentSY->start_year - 1)->first();
+    $lastSYId = $lastSY?->id;
+
+    $enrolledLastYear = Enrollment::where('student_id', $studentId)
+        ->where('school_year_id', $lastSYId)
+        ->exists();
+
+    $previousEnrollments = Enrollment::where('student_id', $studentId)
+        ->where('school_year_id', '<', $schoolYearId)
+        ->exists();
+
+    if ($enrolledLastYear) {
+        Student::where('id', $studentId)->update(['type' => 'regular']);
+    } elseif (!$enrolledLastYear && $previousEnrollments) {
+        Student::where('id', $studentId)->update(['type' => 'returnee']);
+    }
+
+        return static::getModel()::create($data);
     }
 }
