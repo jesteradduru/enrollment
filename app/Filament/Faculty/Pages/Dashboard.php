aa85d580863fead7162bd5f53b0f33efeddee1e1
Jester Adduru <jesteradduru@gmail.com>
2025-06-09 22:16:20 +0800

    Fixed dashboard

--- app/Filament/Faculty/Pages/Dashboard.php
+++ app/Filament/Faculty/Pages/Dashboard.php
@@ -24,13 +24,13 @@ class Dashboard extends \Filament\Pages\Dashboard
             Section::make('Dashboard Filters')->schema([
                 Select::make('school_year')->label('School Year')->options(function(){
                     return SchoolYear::all()->pluck('name', 'id');
-                }),
-                // Select::make('level')->options(function()use($faculty){
-                //     return Level::whereIn('id', $faculty->classrooms()->get()->pluck('level_id'))->get()->pluck('level', 'id');
-                // })->label('Grade Level'),
-                // Select::make('section')->options(function(Get $get)use($faculty){
-                //     return $faculty->classrooms()->whereLike('level_id', $get('level'))->pluck('display_name', 'classroom_id');
-                // }),
+                })->hidden(),
+                Select::make('level')->options(function()use($faculty){
+                    return Level::whereIn('id', $faculty->classrooms()->get()->pluck('level_id'))->get()->pluck('level', 'id');
+                })->label('Grade Level'),
+                Select::make('section')->options(function(Get $get)use($faculty){
+                    return $faculty->classrooms()->whereLike('level_id', $get('level'))->pluck('display_name', 'classroom_id');
+                })->hidden(),
             ])->columns(3)
         ]);
     }
